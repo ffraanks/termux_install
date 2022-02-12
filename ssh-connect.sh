815ffe3f37e3fd8b6899ca59232ff9344651b03b
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-02-11 22:08:40 -0300

    Update ssh-connect.sh

--- ssh-connect.sh
+++ ssh-connect.sh
@@ -12,7 +12,7 @@ if [ $SSH_CHOICE == '1' ] || [ $SSH_CHOICE == '01' ] ; then
   read IP_SSH
   ssh -p 8022 $USER_SSH@$IP_SSH
 
-elif [ $SSH_CHOICE == '1' ] || [ $SSH_CHOICE == '01' ] ; then
+elif [ $SSH_CHOICE == '2' ] || [ $SSH_CHOICE == '02' ] ; then
   clear
   printf "Digite seu user:\n\n"
   read USER_SSH1

d21256bc4218c3940bc2fd738fe3380f0a7c1039
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-02-11 22:00:04 -0300

    Add files via upload

---
+++ ssh-connect.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+clear
+printf "Escolha uma das opções:\n\n[1] - Conectar SSH no celular\n[2] - Conectar SSH no computador:\n[3] - Sair\n\n"
+read SSH_CHOICE
+
+if [ $SSH_CHOICE == '1' ] || [ $SSH_CHOICE == '01' ] ; then
+  clear
+  printf "Digite seu user:\n\n"
+  read USER_SSH
+  printf "\nAgora digite o ip que deseja se conectar:\n\n"
+  read IP_SSH
+  ssh -p 8022 $USER_SSH@$IP_SSH
+
+elif [ $SSH_CHOICE == '1' ] || [ $SSH_CHOICE == '01' ] ; then
+  clear
+  printf "Digite seu user:\n\n"
+  read USER_SSH1
+  printf "\nAgora digite o i'que deseja se conectar:\n\n"
+  read IP_SSH1
+  ssh -p 22 $USER_SSH1@$IP_SSH1
+
+elif [ $SSH_CHOICE == '3' ] || [ $SSH_CHOICE == '03' ] ; then
+  clear && exit 0
+
+else
+  clear && exit 0
+fi
